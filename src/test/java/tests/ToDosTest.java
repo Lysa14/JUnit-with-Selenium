48d838bc9fd811b63977412cbc78efcd461ae30c
Lysa14 <lysaderbah.12@gmail.com>
2024-04-04 11:50:15 +0200

    test ci

--- src/test/java/tests/ToDosTest.java
+++ src/test/java/tests/ToDosTest.java
@@ -1,9 +1,9 @@
 package tests;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,8 +16,8 @@ public class ToDosTest extends BaseTest {
 	HomePage homePage;
 	ToDoPage toDoPage;
 	String[] todos;
-	
-	@BeforeAll
+
+	@BeforeEach
 	public void initialization() {
 		homePage = new HomePage(driver);
 		toDoPage = new ToDoPage(driver);
@@ -26,29 +26,30 @@ public class ToDosTest extends BaseTest {
 
 	@DisplayName("Add and remove todos test")
 	@ParameterizedTest
-    @ValueSource(strings = {"Backbone.js",
-    		"AngularJS",
-    		"Dojo",
-    		"React"})
-    public void userCanAddAndRemoveToDoSuccessfully(String platform) {
-        homePage.openHomePage();
-        homePage.choosePlatform(platform);
-        for(String todo : todos)
-        	toDoPage.addToDo(todo);
-        toDoPage.removeToDo(1);
-        
-        assertEquals(2, toDoPage.itemsLeft());
-        
-        //assertItemsLeft(2, toDoPage.getItemsLeftSpan());
-    }
-	
-//	private void assertItemsLeft(int expectedLeft, WebElement element) {
-//		String expectedTest = (expectedLeft == 1 ) ? String.format("$d item left", expectedLeft) : String.format("$d items left", expectedLeft) ;
-//		validateInnerText(element,expectedTest);
-//    }
-//	
-//	private void validateInnerText(WebElement element, String expectedTest) {
-//		ExpectedConditions.textToBePresentInElement(element, expectedTest);
-//	}
+	@ValueSource(strings = { "Backbone.js",
+			"AngularJS",
+			"Dojo",
+			"React" })
+	public void userCanAddAndRemoveToDoSuccessfully(String platform) {
+		homePage.openHomePage();
+		homePage.choosePlatform(platform);
+		for (String todo : todos)
+			toDoPage.addToDo(todo);
+		toDoPage.removeToDo(1);
+
+		assertEquals(2, toDoPage.itemsLeft());
+
+		// assertItemsLeft(2, toDoPage.getItemsLeftSpan());
+	}
+
+	// private void assertItemsLeft(int expectedLeft, WebElement element) {
+	// String expectedTest = (expectedLeft == 1 ) ? String.format("$d item left",
+	// expectedLeft) : String.format("$d items left", expectedLeft) ;
+	// validateInnerText(element,expectedTest);
+	// }
+	//
+	// private void validateInnerText(WebElement element, String expectedTest) {
+	// ExpectedConditions.textToBePresentInElement(element, expectedTest);
+	// }
 
 }
